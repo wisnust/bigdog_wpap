ccebf217320b2f616660112b67824b92721a0e5e
Wisnu Satrio T <wisnu21cool@gmail.com>
2017-12-31 04:02:07 +0700

    initial all commit

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$('.team-item').magnificPopup({
+    type: 'image',
+    gallery: {
+        enabled: true
+    },
+    image: {
+        verticalFit: true
+    },
+    zoom: {
+        enabled: true,
+        duration: 300, // don't foget to change the duration also in CSS
+        opener: function(element) {
+            return element.find('.bg img');
+        }
+    }
+});
